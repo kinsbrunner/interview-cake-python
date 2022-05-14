0070461d3fbb409dba0ead5e4b81a1db4b415d36
Alejandro Kinsbrunner <kinsbrunner@gmail.com>
2022-05-14 12:11:19 -0300

    Adding test cases for 01.4

--- 01.4-Merged-sorted-arrays.py
+++ 01.4-Merged-sorted-arrays.py
@@ -1,3 +1,5 @@
+import unittest
+
 def merge_lists(my_list, alices_list):
     # Set up our merged_list
     merged_list_size = len(my_list) + len(alices_list)
@@ -29,8 +31,34 @@ def merge_lists(my_list, alices_list):
     return merged_list
 
 
-my_list     = [3, 4, 6, 10, 11, 15]
-alices_list = [1, 5, 8, 12, 14, 19, 19, 20]
+# Tests
 
-# Prints [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19, 19, 20]
-print(merge_lists(my_list, alices_list))
+class Test(unittest.TestCase):
+
+    def test_both_lists_are_empty(self):
+        actual = merge_lists([], [])
+        expected = []
+        self.assertEqual(actual, expected)
+
+    def test_first_list_is_empty(self):
+        actual = merge_lists([], [1, 2, 3])
+        expected = [1, 2, 3]
+        self.assertEqual(actual, expected)
+
+    def test_second_list_is_empty(self):
+        actual = merge_lists([5, 6, 7], [])
+        expected = [5, 6, 7]
+        self.assertEqual(actual, expected)
+
+    def test_both_lists_have_some_numbers(self):
+        actual = merge_lists([2, 4, 6], [1, 3, 7])
+        expected = [1, 2, 3, 4, 6, 7]
+        self.assertEqual(actual, expected)
+
+    def test_lists_are_different_lengths(self):
+        actual = merge_lists([2, 4, 6, 8], [1, 7])
+        expected = [1, 2, 4, 6, 7, 8]
+        self.assertEqual(actual, expected)
+
+
+unittest.main(verbosity=2)
